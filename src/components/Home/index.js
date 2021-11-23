96a2f1af465439e91e8ba9552bee8cd99893b7d7
Abdullah-Aljumah <Abdullah.Ali.elq.js@tuwaiq.edu.sa>
2021-11-23 20:44:49 +0300

    design home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -10,6 +10,53 @@ const Home = () => {
       </div>
       <div className="containerHome">
         <h1 className="welcoming"> HOOOOOOOME </h1>
+        <p className="welcoming">khfaoihfiue</p>
+      </div>
+      <ul className="ulCategory">
+        <li className="categoryLiHome">
+          <div className="categoryHome">
+            <img
+              className="categotyHome"
+              src="https://c.wallhere.com/photos/66/a2/artwork_dark_Venom-1476791.jpg!d"
+              alt="villains"
+            />
+            <div className="textCategory">
+              {" "}
+              <h2 className="h2Category"> Villains </h2>
+            </div>
+          </div>
+          <div></div>
+        </li>
+        <li className="categoryLiHome">
+          <div className="categoryHome">
+            <img
+              className="categotyHome"
+              src="https://c.wallhere.com/photos/66/a2/artwork_dark_Venom-1476791.jpg!d"
+              alt="villains"
+            />
+            <div className="textCategory">
+              {" "}
+              <h2 className="h2Category"> Villains </h2>
+            </div>
+          </div>
+          <div></div>
+        </li>
+        <li className="categoryLiHome">
+          <div className="categoryHome">
+            <img
+              className="categotyHome"
+              src="https://c.wallhere.com/photos/66/a2/artwork_dark_Venom-1476791.jpg!d"
+              alt="villains"
+            />
+            <div className="textCategory">
+              {" "}
+              <h2 className="h2Category"> Villains </h2>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <div>
+        <h1>HOOOOOOOOOOOOLA</h1>
       </div>
     </div>
   );
