a1dcfc9193d8f4c8c0da81c2e713bd2b093dd964
Abdullah-Aljumah <Abdullah.Ali.elq.js@tuwaiq.edu.sa>
2021-11-25 19:50:08 +0300

    add user name in welcoming home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,12 +2,48 @@ import React from "react";
 import Nav from "../Nav";
 import "./style.css";
 import { useNavigate } from "react-router";
-import {MdAccountCircle} from "react-icons/md"
+import { MdAccountCircle } from "react-icons/md";
+import { BsCart4 } from "react-icons/bs";
+import { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
 const Home = () => {
-  const navigate = useNavigate()
-const toCharacter = () => {
-  navigate("/Character")
-}
+  const [account, setAccount] = useState("");
+  const [local, setLocal] = useState([]);
+
+  const getData = async () => {
+
+    const item = await axios.get(
+      `http://localhost:5000/user/email/${local.email}`
+    );
+
+    if (item.data.length > 0) {
+      setAccount(item.data[0].userName);
+    }
+  };
+
+  useEffect(() => {
+      getData();
+ 
+  }, [local]);
+
+  useEffect(() => {
+    getLocalStorage();
+  }, []);
+
+  const getLocalStorage = () => {
+    setLocal(JSON.parse(localStorage.getItem("newUser")));
+  };
+
+  const navigate = useNavigate();
+  const toCharacter = () => {
+    navigate("/Character");
+  };
+
+  const navToAccount = () => {
+    navigate("/account");
+  };
+
   return (
     <div className="backgroundHome">
       <div className="containerHome">
@@ -15,25 +51,43 @@ const toCharacter = () => {
         <div className="temp">
           <ul className="ulTemp">
             <li>
-        <div className="homeContent" >
-        <h1 className="welcoming" id="welcomingH1"> Welocme to "****" </h1>
-        <p className="welcoming" id="pWelcoming"> Looking for some cards?, You are in the right place </p>
-        <h1 className="welcoming" id="getStart" onClick={toCharacter}>Get Started!</h1>
+              <div className="homeContent">
+                <div className="welcomeName">
+                  <h1 className="welcoming" id="welcomingH1">
+                    {" "}
+                    Welocme
+                  </h1>
+                  <h1  id="welcomingH1" className="accountH1"> {account} </h1>
+                </div>
+                <p className="welcoming" id="pWelcoming">
+                  {" "}
+                  Looking for some cards?, You are in the right place{" "}
+                </p>
+                <h1 className="welcoming" id="getStart" onClick={toCharacter}>
+                  Get Started!
+                </h1>
+              </div>
+            </li>
+            <li>
+              <div className="card2Home" onClick={navToAccount}>
+                <h1 className="iconAccount">
+                  {" "}
+                  <MdAccountCircle />
+                </h1>
+                <h2 className="h2Home"> Your Account </h2>
+              </div>
+            </li>
+            <li>
+              <div className="card3Home">
+                <h1 className="iconAccount">
+                  {" "}
+                  <BsCart4 />{" "}
+                </h1>
+                <h2> Cart </h2>
+              </div>
+            </li>
+          </ul>
         </div>
-        </li>
-        <li>
-        <div className="card2Home">
-          <h1> <MdAccountCircle /></h1>
-          <h2> Your Account </h2>
-        </div>
-        </li>
-        <li>
-        <div className="card3Home">
-          <h1> Cart </h1>
-        </div>
-        </li>
-        </ul>
-      </div>
       </div>
     </div>
   );
